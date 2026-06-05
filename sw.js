2393b99e911956e7ce3defdf310f9ab56012e9b5
scanandwingr-hub <scanandwin.gr@gmail.com>
2026-06-05 20:23:40 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'pharmacy-v1';
+const ASSETS = ['/', '/index.html', '/offers.html', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
