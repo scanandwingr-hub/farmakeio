554b1293005c61f43930f1d13b5f72ea94018bad
scanandwingr-hub <scanandwin.gr@gmail.com>
2026-06-07 14:16:59 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'pharmacy-v1';
-const ASSETS = ['/', '/index.html', '/offers.html', '/icon-192.png', '/icon-512.png'];
+const ASSETS = ['/', '/index.html', '/offers.html', '/news.html', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
